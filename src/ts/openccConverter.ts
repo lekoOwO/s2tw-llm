d3948ab31894c8b8292e417284f5eae39f7171c9
Leko <leko@leko.moe>
2025-06-15 06:49:42 +0800

    遷移 opencc-rust

--- src/ts/openccConverter.ts
+++ src/ts/openccConverter.ts
@@ -1,8 +1,9 @@
 // openccConverter.ts
 // 對不含歧義字的句子使用 opencc-js 進行簡轉繁
 
-// @ts-ignore
-import { converter } from "../js/opencc-rust-lib/opencc-rust.mjs";
+import { initOpenccRust, getConverter } from 'opencc-rust';
+await initOpenccRust();
+const converter = getConverter();
 
 export async function convertWithOpenCC(sentence: string): Promise<string> {
   return await converter.convert(sentence);
